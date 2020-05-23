c5df6badfa3fb62340e3312569830c965c9ab5a5
arielnt1993 <61444560+arielnt1993@users.noreply.github.com>
2020-05-23 03:56:07 -0300

    now the number of books of the authors get updated

--- routes/authors/author.route.js
+++ routes/authors/author.route.js
@@ -1,10 +1,21 @@
-const {Router} = require("express");
+const { Router } = require("express");
 const _ = require("lodash");
 const router = Router();
-const authors = require("./authors.json")
+const authors = require("./authors.json");
+const books = require("../books/books.json");
+function updateNumOfBooks(authors, books) {
+  for (let i = 0; i < authors.length; i++) {
+    for (let j = 0; j < books.length; j++) {
+      if (authors[i].fullName == books[j].author) {
+        authors[i].numberOfBooks += 1;
+      }
+    }
+  }
+}
 
-router.get("/authors",(req,res)=>{
-    res.json(authors);
-})
+router.get("/authors", (req, res) => {
+  updateNumOfBooks(authors, books);
+  res.json(authors);
+});
 
 module.exports = router;
